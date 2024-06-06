4bc5a28c230abbf856b4157d4cc4eb03dc5deeba
Eniiyi Bishop Ajala <adedamolaajala33@gmail.com>
2024-06-06 02:19:39 +0100

    Completed Exercises

--- project0.py
+++ project0.py
@@ -94,7 +94,4 @@ def scalar_function(x, y):
     Returns the f(x,y) defined in the problem statement.
     """
     #Your code here
-    if (x <= y):
-        return x * y
-    else:
-        return x / y
+    return x * y if x <= y else x / y
