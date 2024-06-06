4330cbad02b11cc86206046fb446538b69335959
Eniiyi Bishop Ajala <adedamolaajala33@gmail.com>
2024-06-06 15:58:21 +0100

    Debugging Exercise

--- project0.py
+++ project0.py
@@ -116,3 +116,41 @@ def vector_function(x, y):
 
 
 print(vector_function(5, 9))
+
+
+# In this problem, you are given a buggy piece of code and are asked to debug it.
+# The goal of this exercise is for you to set up a working debugging system for yourselves.
+# Feel free to use other debuggers as you wish. But note that any extra print statement in submitted
+# code will be graded as incorrect in the online code graders.
+# The function get_sum_metrics takes two arguments: a prediction and a list of metrics to apply to the prediction
+# (say, for instance, the accuracy or the precision). Note that each metric is a function, not a number. The 
+# function should compute each of the metrics for the prediction and sum them. It should also add to this sum three
+# default metrics, in this case, adding 0, 1 or 2 to the prediction.
+
+# The initial buggy code:
+def get_sum_metrics(predictions, metrics=[]):
+    for i in range(3):
+        metrics.append(lambda x: x + i)
+
+    sum_metrics = 0
+    for metric in metrics:
+        sum_metrics += metric(predictions)
+
+    return sum_metrics
+
+
+# The corrected function:
+def get_sum_metrics(predictions, metrics=None):
+    if metrics is None:
+        metrics = []
+    
+    # Correctly capture the value of i using default arguments in the lambda
+    for i in range(3):
+        metrics.append(lambda x, i=i: x + i)
+
+    sum_metrics = 0
+    for metric in metrics:
+        sum_metrics += metric(predictions)
+
+    return sum_metrics
+
